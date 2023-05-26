be0a6fcdd015977c38c6653a5157bca57df756a9
Anastasiya Soloveva <102226072+Solonasty@users.noreply.github.com>
2023-05-26 15:10:30 +0300

    Add files via upload

---
+++ assets/icons/icons.js
@@ -0,0 +1,20 @@
+// Пример подключения иконки
+// import './svg/my-icon.svg';
+
+import './svg/account.svg';
+import './svg/arrow-right.svg';
+import './svg/comments.svg';
+import './svg/info.svg';
+import './svg/instagram.svg';
+import './svg/like.svg';
+import './svg/person.svg';
+import './svg/question.svg';
+import './svg/search.svg';
+import './svg/star-fill.svg';
+import './svg/youtube.svg';
+import './svg/arrow-left.svg';
+import './svg/cross.svg';
+import './svg/eye.svg';
+import './svg/i.svg';
+import './svg/toggle.svg';
+
---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+/**
+ * Vendors
+ */
+import $ from 'jquery';
+window.$ = $;
+
+// Пример подключения плагина
+// import pluginName from 'plugin-name';
+/**
+ * Modules
+ */
+// Пример подключения модуля
+// import myModule from './modules/my-module-function';
+
+import tabs from './modules/tabs';
+
+const app = {
+    ready() {
+        // Пример вызова импортированнывх функций
+        // pluginName();
+
+        // myModule();
+        tabs();
+        spoiler();
+        details();
+        swiper();
+        select();
+        validate();
+        modal();
+    },
+
+    load() {
+
+    },
+    resize() {
+
+    },
+    scroll() {
+    },
+};
+
+$(() => {
+    app.ready();
+
+    $(window)
+        .on('load', app.load)
+        .on('resize', app.resize)
+        .on('scroll', app.scroll);
+});
